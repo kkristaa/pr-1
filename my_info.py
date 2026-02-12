c88fc03449a2041de2c4fbb679fd02bed4d713ce
“kkristaa” <khknyschuk30@gmail.com>
2026-02-13 00:32:13 +0200

    ввела дані про себе та виконала завдння з зірочкою

---
+++ my_info.py
@@ -0,0 +1,16 @@
+first_name = 'Христина' #str
+last_name = 'Книшук' #str
+age = 16 #int
+height = 1.69 #float
+is_student = True #bool
+
+#Вивід моїх даниз
+print('Моє імʼя', first_name)
+print('Моє прізвище', last_name)
+print('Мій вік', age)
+print('Мій зріст',height)
+print('Чи я студентка',is_student)
+
+#Виконала завдання з зірочкою
+star = int(is_student)
+print('Число з boolean', star)
